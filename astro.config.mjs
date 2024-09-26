f5f68f9a23f4b116d2e365d80a7f3477856f20bb
MaathavanJkr <maathumaathavan@gmail.com>
2024-09-26 22:17:05 +0530

    nic to index update

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,9 +11,11 @@ export default defineConfig({
 	output: "hybrid",
 	redirects: {
 		"/": `/${defaultLang}/`,
-		"/modelpaper": "https://drive.google.com/drive/folders/1pAzO3OQ7nwBLuJ_gU-4vY12x6eENrwPz",
+		"/modelpaper":
+			"https://drive.google.com/drive/folders/1pAzO3OQ7nwBLuJ_gU-4vY12x6eENrwPz",
 		"/feedback": "https://forms.gle/Ca6k2rH6FNCn6NTB8",
 		"/results": `/${defaultLang}/results`,
+		"/indexnumber": `/${defaultLang}/indexnumber`,
 		"/test": "https://google.com",
 		"/practicepapers": `/${defaultLang}/practicepapers`,
 		"/instructions": `/${defaultLang}/instructions_ta`,
@@ -23,7 +25,6 @@ export default defineConfig({
 		"/manage": "https://moraetamils.com/manage",
 		"/tshirt": "https://forms.gle/SJTHD5c5kqkegARk9",
 		"/moraexamadmissionform": "https://forms.gle/shTKWksUk7KSkjrV6",
-
 	},
 	adapter: netlify({
 		edgeMiddleware: true,
