5fed8a2a5da3d12274e4bd2136ccc73ce3beab6d
KRakeesh04 <kanesrakeshan@gmail.com>
2025-08-08 23:09:24 +0530

    fix: student registration service response body change

--- src/services/studentRegistrationService.ts
+++ src/services/studentRegistrationService.ts
@@ -1,50 +1,66 @@
 const API_URL = import.meta.env.PUBLIC_API_URL;
 
 if (!API_URL) {
-	throw new Error("PUBLIC_API_URL is not defined in the environment variables.");
+	throw new Error(
+		"PUBLIC_API_URL is not defined in the environment variables.",
+	);
 }
 
 interface StudentRegistrationDetails {
-	name: string,
-	stream_id: number,
-	medium: string,
-	rank_district_id: number,
-	exam_district_id: number,
-	exam_centre_id: number,
-	nic: string,
-	gender: string,
-	email: string,
-	telephone_no: string,
-	school: string,
-	address: string,
+	name: string;
+	stream_id: number;
+	medium: string;
+	rank_district_id: number;
+	exam_district_id: number;
+	exam_centre_id: number;
+	nic: string;
+	gender: string;
+	email: string;
+	telephone_no: string;
+	school: string;
+	address: string;
+	transaction_file: File;
 }
 
-export const registerStudent = async (
-	details: StudentRegistrationDetails
-) => {
+export const registerStudent = async (details: StudentRegistrationDetails) => {
 	try {
 		details.name = details.name.trim().toUpperCase();
 		details.school = details.school.trim().toUpperCase();
 		details.address = details.address.trim().toUpperCase();
 
-		const response = await fetch(API_URL.concat("/temp-student/add"), {
+		const data = new FormData();
+		data.set("nic", details.nic);
+		data.set("name", details.name);
+		data.set("school", details.school);
+		data.set("address", details.address);
+		data.set("email", details.email);
+		data.set("telephone_no", details.telephone_no);
+		data.set("gender", details.gender);
+		data.set("medium", details.medium);
+		data.set("stream_id", String(details.stream_id));
+		data.set("rank_district_id", String(details.rank_district_id));
+		data.set("exam_district_id", String(details.exam_district_id));
+		data.set("exam_centre_id", String(details.exam_centre_id));
+		data.set("transaction_file", details.transaction_file);
+
+		const response = await fetch(API_URL.concat("/student-registration/add"), {
 			method: "POST",
-			body: JSON.stringify(details),
-			headers: {
-				"Content-Type": "application/json",
-			},
-		})
-		
+			body: data,
+			// headers: {
+			// 	"Content-Type": "application/json",
+			// },
+		});
+
 		if (!response.ok) {
 			return "Failed to register. Please try again.";
 		}
 	} catch (error) {
 		console.error("Error Adding Student:", error);
-		
+
 		if (error instanceof Error) {
 			return error.message;
 		}
-		
+
 		return "An unexpected error occurred. Please try again.";
 	}
 };
