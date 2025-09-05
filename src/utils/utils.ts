6ca0e8f8a164658cf6456dcbfcc3aa44eb7920e0
Sahithyan <sahithyan2701@gmail.com>
2025-09-05 10:29:02 +0530

    fix: bug with gender specific centres

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -107,3 +107,9 @@ export function groupPastPaperItems(items: Array<PastPaperItem>) {
 
 	return grouped;
 }
+
+export function humanReadableFileSize(size: number): string {
+	if (size < 1024) return `${size} bytes`;
+	else if (size < 1048576) return `${(size / 1024).toFixed(2)} KB`;
+	else return `${(size / 1048576).toFixed(2)} MB`;
+}
